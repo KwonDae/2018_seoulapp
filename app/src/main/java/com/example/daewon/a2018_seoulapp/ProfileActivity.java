94e09b6a7ea30df48fed108fd3d480b23a247803
KwonDae <goodday94@cnu.ac.kr>
2018-08-09 16:39:36 +0900

    test

--- app/src/main/java/com/example/daewon/a2018_seoulapp/ProfileActivity.java
+++ app/src/main/java/com/example/daewon/a2018_seoulapp/ProfileActivity.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ProfileActivity extends BaseActivity implements View.OnClickListener{
+public class ProfileActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = "ProfileActivity";
 
     //firebase auth object
@@ -41,7 +41,7 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
         //유저가 로그인 하지 않은 상태라면 null 상태이고 이 액티비티를 종료하고 로그인 액티비티를 연다.
-        if(firebaseAuth.getCurrentUser() == null) {
+        if (firebaseAuth.getCurrentUser() == null) {
             finish();
             startActivity(new Intent(this, LoginActivity.class));
         }
@@ -50,7 +50,7 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         //textViewUserEmail의 내용을 변경해 준다.
-        textViewUserEmail.setText("반갑습니다.\n"+ user.getEmail()+"으로 로그인 하였습니다.");
+        textViewUserEmail.setText("반갑습니다.\n" + user.getEmail() + "으로 로그인 하였습니다.");
 
         //logout button event
         buttonLogout.setOnClickListener(this);
@@ -64,7 +64,7 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        }, 2000);
 
     }
 
@@ -76,7 +76,7 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
             startActivity(new Intent(this, LoginActivity.class));
         }
         //회원탈퇴를 클릭하면 회원정보를 삭제한다. 삭제전에 컨펌창을 하나 띄워야 겠다.
-        if(view == textivewDelete) {
+        if (view == textivewDelete) {
             AlertDialog.Builder alert_confirm = new AlertDialog.Builder(ProfileActivity.this);
             alert_confirm.setMessage("정말 계정을 삭제 할까요?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
                         @Override
