6b92d998fb5f23cf31e4efad80eb25d66d234e0d
JeonHyungJin <qpdjgjsl@gmail.com>
2018-09-14 22:32:06 +0900

    design change

--- app/src/main/java/com/example/daewon/a2018_seoulapp/Activity/MapActivity.java
+++ app/src/main/java/com/example/daewon/a2018_seoulapp/Activity/MapActivity.java
@@ -8,102 +8,141 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.daewon.a2018_seoulapp.R;
 
 public class MapActivity extends BaseActivity {
 
-    Intent intent = new Intent(this,Map_sub_Activity.class);
+    //Intent intent = new Intent(this,Map_sub_Activity.class);
     private long mLastClickTime;
+    TextView location_text;
+    Intent To_detail_list_intent;
+    Button To_detail_button;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        location_text = (TextView)findViewById(R.id.location_text);
+        To_detail_button = (Button)findViewById(R.id.Button_todetail);
+
+        To_detail_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(To_detail_list_intent);
+            }
+        });
     }
 
     public void clickbtn(View v){
+
         if(SystemClock.elapsedRealtime()-mLastClickTime < 1000){
             return;
         }
+
         mLastClickTime = SystemClock.elapsedRealtime();
-        Intent intent = new Intent(this,Map_sub_Activity.class);
+        To_detail_list_intent = new Intent(this,Map_sub_Activity.class);
         switch (v.getId()){
             case R.id.dobong:
-                intent.putExtra("name","도봉구");
+                location_text.setText("도봉구");
+                To_detail_list_intent.putExtra("name","도봉구");
                 break;
             case R.id.gangbook:
-                intent.putExtra("name","강북구");
+                location_text.setText("강북구");
+                To_detail_list_intent.putExtra("name","강북구");
                 break;
             case R.id.jongro:
-                intent.putExtra("name","종로구");
+                location_text.setText("종로구");
+                To_detail_list_intent.putExtra("name","종로구");
                 break;
             case R.id.nowon:
-                intent.putExtra("name","노원구");
+                location_text.setText("노원구");
+                To_detail_list_intent.putExtra("name","노원구");
                 break;
             case R.id.eunpyoung:
-                intent.putExtra("name","은평구");
+                location_text.setText("은평구");
+                To_detail_list_intent.putExtra("name","은평구");
                 break;
             case R.id.seodamoon:
-                intent.putExtra("name","서대문구");
+                location_text.setText("서대문구");
+                To_detail_list_intent.putExtra("name","서대문구");
                 break;
             case R.id.mapo:
-                intent.putExtra("name","마포구");
+                location_text.setText("마포구");
+                To_detail_list_intent.putExtra("name","마포구");
                 break;
             case R.id.gangseo:
-                intent.putExtra("name","강서구");
+                location_text.setText("강서구");
+                To_detail_list_intent.putExtra("name","강서구");
                 break;
             case R.id.yangcheon:
-                intent.putExtra("name","양천구");
+                location_text.setText("양천구");
+                To_detail_list_intent.putExtra("name","양천구");
                 break;
             case R.id.ydp:
-                intent.putExtra("name","영등포구");
+                location_text.setText("영등포구");
+                To_detail_list_intent.putExtra("name","영등포구");
                 break;
             case R.id.guro:
-                intent.putExtra("name","구로구");
+                location_text.setText("구로구");
+                To_detail_list_intent.putExtra("name","구로구");
                 break;
             case R.id.guemcheon:
-                intent.putExtra("name","금천구");
+                location_text.setText("금천구");
+                To_detail_list_intent.putExtra("name","금천구");
                 break;
             case R.id.dongjak:
-                intent.putExtra("name","동작구");
+                location_text.setText("동작구");
+                To_detail_list_intent.putExtra("name","동작구");
                 break;
             case R.id.yoongsan:
-                intent.putExtra("name","용산구");
+                location_text.setText("용산구");
+                To_detail_list_intent.putExtra("name","용산구");
                 break;
             case R.id.jung:
-                intent.putExtra("name","중구");
+                location_text.setText("중구");
+                To_detail_list_intent.putExtra("name","중구");
                 break;
             case R.id.gangnam:
-                intent.putExtra("name","강남구");
+                location_text.setText("강남구");
+                To_detail_list_intent.putExtra("name","강남구");
                 break;
             case R.id.seocho:
-                intent.putExtra("name","서초구");
+                location_text.setText("서초구");
+                To_detail_list_intent.putExtra("name","서초구");
                 break;
             case R.id.sungdong:
-                intent.putExtra("name","성동구");
+                location_text.setText("성동구");
+                To_detail_list_intent.putExtra("name","성동구");
                 break;
             case R.id.jungrang:
-                intent.putExtra("name","중랑구");
+                location_text.setText("중랑구");
+                To_detail_list_intent.putExtra("name","중랑구");
                 break;
             case R.id.gangdong:
-                intent.putExtra("name","강동구");
+                location_text.setText("강동구");
+                To_detail_list_intent.putExtra("name","강동구");
                 break;
             case R.id.gwangjin:
-                intent.putExtra("name","광진구");
+                location_text.setText("광진구");
+                To_detail_list_intent.putExtra("name","광진구");
                 break;
             case R.id.gwanak:
-                intent.putExtra("name","관악구");
+                location_text.setText("관악구");
+                To_detail_list_intent.putExtra("name","관악구");
                 break;
             case R.id.dongdamoon:
-                intent.putExtra("name","동대문구");
+                location_text.setText("동대문구");
+                To_detail_list_intent.putExtra("name","동대문구");
                 break;
             case R.id.sungbook:
-                intent.putExtra("name","성북구");
+                location_text.setText("성북구");
+                To_detail_list_intent.putExtra("name","성북구");
                 break;
             case R.id.songpa:
-                intent.putExtra("name","송파구");
+                location_text.setText("송파구");
+                To_detail_list_intent.putExtra("name","송파구");
                 break;
         }
-        startActivity(intent);
     }
 }
