586b8e9e96c9eb69823fefe16ddc9734f1bdc0b9
JeonHyungJin <qpdjgjsl@gmail.com>
2018-09-14 23:12:28 +0900

    지역별로 리스트

--- app/src/main/java/com/example/daewon/a2018_seoulapp/Activity/Map_sub_Activity.java
+++ app/src/main/java/com/example/daewon/a2018_seoulapp/Activity/Map_sub_Activity.java
@@ -1,34 +1,171 @@
 package com.example.daewon.a2018_seoulapp.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.daewon.a2018_seoulapp.GalleryList;
+import com.example.daewon.a2018_seoulapp.ImageDTO;
 import com.example.daewon.a2018_seoulapp.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Map_sub_Activity extends BaseActivity {
-    private List<My_gallery_read_data> my_gallery_read_datas = new ArrayList<>();
-    FirebaseDatabase database;
-    FirebaseAuth firebaseAuth;
+    private RecyclerView recyclerView;
+    private List<ImageDTO> location_list = new ArrayList<>();
+    private List<String>uidLists = new ArrayList<>();
+    private FirebaseDatabase database;
+    private FirebaseAuth firebaseAuth;
+    String region;
+
+    private String temp1,temp2,temp3,temp4,temp5;
+    private int count=0;
+
+    private ImageButton best5, find_gallery, mypage;
+    int i = 1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_sub);
-        database = FirebaseDatabase.getInstance();
-        firebaseAuth = FirebaseAuth.getInstance();
+
 
         Intent intent = getIntent();
 
         if(intent != null){
-            String region = intent.getStringExtra("name");
+            region = intent.getStringExtra("name");
 
 
         }
 
+        database = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycleview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final BoardRecylcerViewAdapter boardRecylcerViewAdapter = new BoardRecylcerViewAdapter();
+        recyclerView.setAdapter(boardRecylcerViewAdapter);
+
+        best5 = findViewById(R.id.best5);
+        find_gallery = findViewById(R.id.find_gallery);
+        mypage = findViewById(R.id.mypage);
+
+        database.getReference().child("Gallerys").child(region).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                location_list.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    ImageDTO imageDTO = snapshot.getValue(ImageDTO.class);
+                    //System.out.println(my_gallery_read_data.My_Gallery_name);
+                    location_list.add(imageDTO);
+                }
+                boardRecylcerViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        best5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i != 1) {
+                    best5.setImageResource(R.drawable.best5_on);
+                    find_gallery.setImageResource(R.drawable.gallery_off);
+                    mypage.setImageResource(R.drawable.mypage_off);
+                    Intent intent = new Intent(Map_sub_Activity.this, GalleryList.class);
+                    startActivity(intent);
+                    i =1;
+                }
+            }
+        });
+
+        find_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i != 2) {
+                    best5.setImageResource(R.drawable.best5_off);
+                    find_gallery.setImageResource(R.drawable.gallery_on);
+                    mypage.setImageResource(R.drawable.mypage_off);
+                    i = 2;
+                    Intent intent = new Intent(Map_sub_Activity.this, MapActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i != 3) {
+                    best5.setImageResource(R.drawable.best5_off);
+                    find_gallery.setImageResource(R.drawable.gallery_off);
+                    mypage.setImageResource(R.drawable.mypage_on);
+                    i = 3;
+                }
+            }
+        });
+    }
+
+    class BoardRecylcerViewAdapter extends  RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_gallery,parent,false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position){
+            ((CustomViewHolder)holder).textView.setText(location_list.get(position).Gallery_name);
+            ((CustomViewHolder)holder).textView2.setText(location_list.get(position).Gallery_location_from_list);
+            Glide.with(holder.itemView.getContext()).load(location_list.get(position).Main_img).into(((CustomViewHolder)holder).imageView);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String url = location_list.get(position).Main_img;
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return location_list.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+            TextView textView;
+            TextView textView2;
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = view.findViewById(R.id.item_imageView);
+                textView = view.findViewById(R.id.item_textView);
+                textView2 = view.findViewById(R.id.item_textView2);
+            }
+        }
     }
 }
