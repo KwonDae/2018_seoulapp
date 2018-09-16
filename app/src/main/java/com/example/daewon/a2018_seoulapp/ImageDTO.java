ebb1d010866e0eed186890ebcfdf894986e17c93
KwonDae <goodday94@cnu.ac.kr>
2018-09-16 17:51:58 +0900

    Star Map_sub_Activity

--- app/src/main/java/com/example/daewon/a2018_seoulapp/ImageDTO.java
+++ app/src/main/java/com/example/daewon/a2018_seoulapp/ImageDTO.java
@@ -13,8 +13,9 @@ public class ImageDTO {
     public String Gallery_time;
     public String Owner_explain;
     public String Owner_insta;
+    public String Gallery_owner_email;
     public int starCount = 0;
     public Map<String, Boolean> stars = new HashMap<>();
-    public Map<String, String> Comments = new HashMap<>();
+    public Map<String, comment_data> Comments = new HashMap<>();
     public Map<String, String> Gallery_imgs = new HashMap<>();
 }
